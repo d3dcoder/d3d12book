81cc00f708f1e28534e23a47b6cb322001bd38fd
ohmaya <ohmaya@163.com>
2019-12-22 13:58:04 +0800

    nonuniform scaling => uniform scaling
    gWorld instead of inverse-transpose of world matrix is used, so you were actually assuming "uniform scaling".

--- Chapter 21 Ambient Occlusion/Ssao/Shaders/DrawNormals.hlsl
+++ Chapter 21 Ambient Occlusion/Ssao/Shaders/DrawNormals.hlsl
@@ -41,7 +41,7 @@ VertexOut VS(VertexIn vin)
 	// Fetch the material data.
 	MaterialData matData = gMaterialData[gMaterialIndex];
 	
-    // Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
+    // Assumes uniform scaling; otherwise, need to use inverse-transpose of world matrix.
     vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
 	vout.TangentW = mul(vin.TangentU, (float3x3)gWorld);
 
